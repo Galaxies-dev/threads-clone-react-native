a560bfbbbd7f35b55f8d238e270bc0d519fa5f17
Simon Grimm <saimon@devdactic.com>
2024-09-27 09:09:24 +0200

    Uncomment debug thing.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -78,7 +78,7 @@ const InitialLayout = () => {
     const inTabsGroup = segments[0] === '(auth)';
 
     if (isSignedIn && !inTabsGroup) {
-      // router.replace('/(auth)/(tabs)/feed');
+      router.replace('/(auth)/(tabs)/feed');
     } else if (!isSignedIn && inTabsGroup) {
       router.replace('/(public)');
     }
